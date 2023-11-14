3aaeaec00b2b6662af1c0b95b095789213b81e6e
nxhdev2002 <xuanhoangv7c@gmail.com>
2023-11-14 22:57:07 +0700

    update

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,9 +1,12 @@
 import multer from 'multer'
+import fs from 'fs'
 
 // SET STORAGE
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads')
+  destination: (req, file, cb) => {
+    const path = 'uploads'
+    fs.mkdirSync(path, { recursive: true })
+    return cb(null, path)
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname)
