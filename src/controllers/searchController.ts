3940d9be0545915c4cb05bbdec9a87c2506c1ad5
Minh <maiankhanh1234@gmail.com>
2023-12-16 21:28:59 +0700

    search music #1

---
+++ src/controllers/searchController.ts
@@ -0,0 +1,28 @@
+/* eslint-disable eol-last */
+/* eslint-disable @typescript-eslint/indent */
+import { type Request, type Response } from 'express'
+import { type HttpResponse } from '@/types/HttpResponse'
+import catchAsyncError from '@/middlewares/catchAsyncError'
+import sequelize from '@/utils/dbConn'
+interface MusicPath {
+    Id: number
+    Name: string
+    FilePath: string
+    Quality: string
+  }
+export const searchMusicFileById = catchAsyncError(async (req: Request, res: Response): Promise<void> => {
+    const id = req.params.id
+    const [results] = await sequelize.query('call HMA_APP_MUSIC_GET_INFO_BY_ID(:id)', {
+      replacements: {
+        id
+      }
+    }) as unknown as [MusicPath, any]
+    if (results === null) {
+      const resp: HttpResponse<any> = {
+        status: 404,
+        message: 'File not found'
+      }
+      res.status(404).send(resp)
+    }
+    res.download(results.FilePath)
+  })
--- src/routes/app-routing.ts
+++ src/routes/app-routing.ts
@@ -7,6 +7,7 @@ import upload from '@/middlewares/upload'
 import { googleLoginValidator, loginValidator } from '@/validator/loginValidator'
 import { registerValidator } from '@/validator/registerValidator'
 import { changePasswordValidator } from '@/validator/changePasswordValidator'
+import { searchMusicFileById } from '@/controllers/searchController'
 
 const route = Router()
 
@@ -23,6 +24,7 @@ route.route('/get-otp').get(isAuthenticatedUser, getOTP)
 
 /// music routes
 route.route('/song/:id/download').get(downloadMusicFileById)
+route.route('/song/:id/search').get(searchMusicFileById)
 
 /// App version
 route.route('/apk/upload').post(upload.single('file'), uploadAPKFile)
